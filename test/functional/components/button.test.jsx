a9602586f1ebf763cb4aadb6b580233701c1c5e5
Kenny Heaton <kenny.heaton@life.church>
2018-04-21 21:37:32 -0500

    Add TextField

--- test/functional/components/button.test.jsx
+++ test/functional/components/button.test.jsx
@@ -1,5 +1,5 @@
 import Adapter from 'enzyme-adapter-react-16';
-import Button from '../../src/components/button';
+import Button from '../../../src/components/button';
 import Enzyme from 'enzyme';
 import React from 'react';
 import sinon from 'sinon';
---
+++ test/functional/components/text-field.test.jsx
@@ -0,0 +1,22 @@
+import Adapter from 'enzyme-adapter-react-16';
+import TextField from '../../../src/components/text-field';
+import Enzyme from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('TextField keyup on typeing', () => {
+  const onKeyUp = sinon.spy();
+  const wrapper = Enzyme.shallow(<TextField onInput={onKeyUp} />);
+  const event = { target: { value: 'Love' } };
+  const input = wrapper.find('input');
+  input.simulate('keyup', event);
+  expect(onKeyUp.calledWith()).toBeTruthy()
+});
+
+test('TextField has value', () => {
+  const wrapper = Enzyme.shallow(<TextField value="Love" />);
+  const input = wrapper.find('input');
+  expect(input.props().value).toEqual("Love");
+});
