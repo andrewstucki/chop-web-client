6bcd78ab195bb8a615dd0f7813464ae6ca0d5e9d
Kenny Heaton <kenny.heaton@life.church>
2018-05-08 16:49:35 -0500

    Fix CSS Modules

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { 
+            loader: 'css-loader',
+            options: { 
+              module: true,
+              importLoaders: 1 
+            }
+          },
           'postcss-loader'
         ]
       },
