1c8c73b6487ae4dbbd576eab53aef7b761a9119d
Kenny Heaton <kenny.heaton@life.church>
2018-05-10 14:24:39 -0500

    Adding Stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ function loadStories() {
   require('../stories/button.jsx');
   require('../stories/text-field.jsx');
   require('../stories/chat.jsx');
+  require('../stories/feed.jsx')
 }
 
 configure(loadStories, module);
