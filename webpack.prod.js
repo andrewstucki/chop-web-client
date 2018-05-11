9aaf63bed156467834b0dba5979bbaf96c83905e
Kenny Heaton <kenny.heaton@life.church>
2018-05-10 21:33:57 -0500

    Adding a manifest and PWA settings

---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new CompressionPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'assets/manifest.webmanifest', to: 'manifest.webmanifest' },
+    ]),
+  ]
+});
+
