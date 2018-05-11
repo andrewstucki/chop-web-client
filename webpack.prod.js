201e0e36113f69735cfb1d7a94f145c1449df58a
Kenny Heaton <kenny.heaton@life.church>
2018-05-10 20:11:16 -0500

    Making it fast

---
+++ webpack.prod.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new CompressionPlugin(),
+  ]
+});
+
