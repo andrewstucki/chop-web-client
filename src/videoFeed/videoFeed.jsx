9f5ac508f06225976a9b7b5e22b7923bd30fe8dc
Kenny Heaton <kennyheaton@gmail.com>
2018-05-24 16:17:08 -0500

    Adding Video

--- src/videoFeed/videoFeed.jsx
+++ src/videoFeed/videoFeed.jsx
@@ -1,15 +1,23 @@
 // @flow
 import React from 'react';
-import videoFeedStyles from './styles.css';
+import styles from './styles.css';
 
 type VideoFeedProps = {
   isVideoHidden: boolean,
 };
 
 const VideoFeed = ({isVideoHidden}: VideoFeedProps) => {
-  const style = isVideoHidden ? videoFeedStyles.hideVideo : videoFeedStyles.showVideo;
+  const style = isVideoHidden ? styles.hideVideo : styles.showVideo;
   return (
-    <div className={style}></div>
+    <div className={style}>
+      <iframe
+        className={styles.frame}
+        src="https://www.youtube.com/embed/bz2kN31m_S0?rel=0"
+        width="550"
+        height="281"
+        frameborder="0"
+      ></iframe>
+    </div>
   );
 };
 
