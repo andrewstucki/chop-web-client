95a186404fba10703b791b31049706a1ddf59af9
Houston Green <greenhouston@gmail.com>
2018-06-13 14:18:36 -0500

    added swipe functionality

--- src/message/message.jsx
+++ src/message/message.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import ReactTouchEvents from 'react-touch-events';
 
 import type { MessageType } from './dux';
 import { getFirstInitial, getAvatarColor } from '../util';
@@ -25,32 +26,38 @@ const Message = (
   const style = appendingMessage ? styles.appending : styles.notAppending;
 
   return (
-    <div className={style}>
-      <div
-        className={styles.icon} 
-        style={{backgroundColor: getAvatarColor(message.user.nickname)}}
-      >
-        {getFirstInitial(message.user.nickname)}
-      </div>
-      <div className={styles.body}>
-        <strong className={styles.name}>{message.user.nickname}</strong>
-        <span className={styles.role}>Host</span>
-        <div className={styles.message}>{message.text}</div>
-      </div>
-      <button
-        className={styles.openTrayButton}
-        dangerouslySetInnerHTML={{ __html: OpenTrayButton }}
-        onClick={() => {
-          messageButtonOnClick(message.id);
-        }}
-      />
-        <MessageTray
-          messageTrayOpen={message.messageTrayOpen}
-          trayButtonOnClick={() => {
-            trayButtonOnClick(message.id);
+    <ReactTouchEvents
+      onSwipe={direction => {
+        if (direction === 'left') return messageButtonOnClick(message.id);
+      }}
+    >
+      <div className={style}>
+        <div
+          className={styles.icon} 
+          style={{backgroundColor: getAvatarColor(message.user.nickname)}}
+        >
+          {getFirstInitial(message.user.nickname)}
+        </div>
+        <div className={styles.body}>
+          <strong className={styles.name}>{message.user.nickname}</strong>
+          <span className={styles.role}>Host</span>
+          <div className={styles.message}>{message.text}</div>
+        </div>
+        <button
+          className={styles.openTrayButton}
+          dangerouslySetInnerHTML={{ __html: OpenTrayButton }}
+          onClick={() => {
+            messageButtonOnClick(message.id);
           }}
         />
-    </div>
+          <MessageTray
+            messageTrayOpen={message.messageTrayOpen}
+            trayButtonOnClick={() => {
+              trayButtonOnClick(message.id);
+            }}
+          />
+      </div>
+    </ReactTouchEvents>
   );
 };
 
