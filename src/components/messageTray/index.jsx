7543e39d330381179107ed1b1bdbb8a86bcd5883
Houston Green <greenhouston@gmail.com>
2018-06-18 11:01:23 -0500

    removed swipe and gave tray a fixed height

--- src/components/messageTray/index.jsx
+++ src/components/messageTray/index.jsx
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import ReactTouchEvents from 'react-touch-events';
 
 import DirectChatButton from '../../../assets/direct-chat-button.svg';
 import DeleteButton from '../../../assets/delete-button.svg';
@@ -45,50 +44,44 @@ const MessageTray = (
   const trayStyle = messageTrayOpen ? styles.open : styles.closed;
 
   return (
-    <ReactTouchEvents
-      onSwipe={direction => {
-        if (direction === 'right') return closeMessageTray(messageId);
-      }}
-    >
-      <div className={trayStyle}>
-        <button
-          className={styles.closeMessageTray}
-          onClick={() => {
-            closeMessageTray(messageId);
-          }}
-        >
-          <span 
-            dangerouslySetInnerHTML={{ __html: CloseMessageTray }}
-            className={styles.closeTrayImage}
-          />
-        </button>
-        {
-          trayButton(
-            styles.directChatButton,
-            styles.directChatImage,
-            DirectChatButton,
-            'Direct chat'
-          )
-        }
-        {
-          trayButton(
-            styles.deleteButton,
-            styles.deleteImage,
-            DeleteButton,
-            'Delete message',
-            deleteMessage
-          )
-        }
-        {
-          trayButton(
-            styles.muteButton,
-            styles.muteImage,
-            MuteButton,
-            'Mute guest'
-          )
-        }
-      </div>
-    </ReactTouchEvents>
+    <div className={trayStyle}>
+      <button
+        className={styles.closeMessageTray}
+        onClick={() => {
+          closeMessageTray(messageId);
+        }}
+      >
+        <span 
+          dangerouslySetInnerHTML={{ __html: CloseMessageTray }}
+          className={styles.closeTrayImage}
+        />
+      </button>
+      {
+        trayButton(
+          styles.directChatButton,
+          styles.directChatImage,
+          DirectChatButton,
+          'Direct chat'
+        )
+      }
+      {
+        trayButton(
+          styles.deleteButton,
+          styles.deleteImage,
+          DeleteButton,
+          'Delete message',
+          deleteMessage
+        )
+      }
+      {
+        trayButton(
+          styles.muteButton,
+          styles.muteImage,
+          MuteButton,
+          'Mute guest'
+        )
+      }
+    </div>
   );
 };
 
--- src/message/message.jsx
+++ src/message/message.jsx
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import ReactTouchEvents from 'react-touch-events';
 
 import type { MessageType } from './dux';
 import { getFirstInitial, getAvatarColor } from '../util';
@@ -28,40 +27,34 @@ const Message = (
   const messageStyle = appendingMessage ? styles.appending : styles.notAppending;
 
   return (
-    <ReactTouchEvents
-      onSwipe={direction => {
-        if (direction === 'left') return openMessageTray(message.id);
-      }}
-    >
-      <div data-component="message" className={messageStyle}>
-        <div
-          className={styles.icon} 
-          style={{backgroundColor: getAvatarColor(message.user.nickname)}}
-        >
-          {getFirstInitial(message.user.nickname)}
-        </div>
-        <div className={styles.body}>
-          <strong className={styles.name}>{message.user.nickname}</strong>
-          <span className={styles.role}>Host</span>
-          <div data-node="text" className={styles.message}>{message.text}</div>
-        </div>
-        <button
-          className={styles.openTrayButton}
-          dangerouslySetInnerHTML={{ __html: OpenTrayButton }}
-          onClick={() => {
-            openMessageTray(message.id);
-          }}
-        />
-        <MessageTray
-          messageId={message.id}
-          messageTrayOpen={message.messageTrayOpen}
-          closeMessageTray={closeMessageTray}
-          deleteMessage={() => {
-            deleteMessage(message.id);
-          }}
-        />
+    <div data-component="message" className={messageStyle}>
+      <div
+        className={styles.icon} 
+        style={{backgroundColor: getAvatarColor(message.user.nickname)}}
+      >
+        {getFirstInitial(message.user.nickname)}
       </div>
-    </ReactTouchEvents>
+      <div className={styles.body}>
+        <strong className={styles.name}>{message.user.nickname}</strong>
+        <span className={styles.role}>Host</span>
+        <div data-node="text" className={styles.message}>{message.text}</div>
+      </div>
+      <button
+        className={styles.openTrayButton}
+        dangerouslySetInnerHTML={{ __html: OpenTrayButton }}
+        onClick={() => {
+          openMessageTray(message.id);
+        }}
+      />
+      <MessageTray
+        messageId={message.id}
+        messageTrayOpen={message.messageTrayOpen}
+        closeMessageTray={closeMessageTray}
+        deleteMessage={() => {
+          deleteMessage(message.id);
+        }}
+      />
+    </div>
   );
 };
 
