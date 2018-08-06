85bf3112713acbba500321221dbff2a2cf212a7c
Houston Green <greenhouston@gmail.com>
2018-08-06 09:30:09 -0500

    chat component is a class

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,6 +1,6 @@
 // @flow
 /* global SyntheticMouseEvent, SyntheticKeyboardEvent, SyntheticFocusEvent */
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../components/button';
 import TextField from '../components/text-field';
 import UpArrow from '../../assets/large-arrow-up.svg';
@@ -18,44 +18,44 @@ type ChatProps = {
   currentPlaceholder: string,
 };
 
-const Chat =
-(
-  {
-    buttonOnClick,
-    textOnInput,
-    textOnBlur,
-    textOnFocus,
-    textValue,
-    textEntered = false,
-    focused = false,
-    enterDetect,
-    currentPlaceholder,
-  }: ChatProps
-) => {
-  const style = focused ? styles.focused : styles.default;
+class Chat extends Component<ChatProps> {
+  render () {
+    const {
+      buttonOnClick,
+      textOnInput,
+      textOnBlur,
+      textOnFocus,
+      textValue,
+      textEntered = false,
+      focused = false,
+      enterDetect,
+      currentPlaceholder,
+    } = this.props;
 
-  return (
-    <div className={styles.background}>
-      <div className={style}>
-        <TextField
-          onInput={textOnInput}
-          onBlur={textOnBlur}
-          onFocus={textOnFocus}
-          value={textValue}
-          placeholder={currentPlaceholder}
-          enterDetect={enterDetect}
-        />
-        {textEntered &&
-          <Button
-            onClick={buttonOnClick}
-            image={UpArrow}
-            buttonType="icon"
-            imageType="arrow"
+    const style = focused ? styles.focused : styles.default;
+    return (
+      <div className={styles.background}>
+        <div className={style}>
+          <TextField
+            onInput={textOnInput}
+            onBlur={textOnBlur}
+            onFocus={textOnFocus}
+            value={textValue}
+            placeholder={currentPlaceholder}
+            enterDetect={enterDetect}
           />
-        }
+          {textEntered &&
+            <Button
+              onClick={buttonOnClick}
+              image={UpArrow}
+              buttonType="icon"
+              imageType="arrow"
+            />
+          }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Chat;
