2eacbcbf9e8bdcb2ed528af37b75ef236c2a4db8
Houston Green <greenhouston@gmail.com>
2018-08-09 14:09:03 -0500

    re removed reducers from chop dux

--- src/chop/dux.js
+++ src/chop/dux.js
@@ -3,8 +3,6 @@ import feedReducer from '../feed/dux';
 import ioReducer from '../io/dux';
 import videoFeedReducer from '../videoFeed/dux';
 import sideMenuReducer from '../sideMenu/dux';
-import momentReducer from '../moment/dux';
-import placeholderReducer from '../placeholder/dux';
 import { combineReducers } from 'redux';
 
 const reducer = combineReducers({
@@ -12,8 +10,6 @@ const reducer = combineReducers({
   io: ioReducer,
   videoFeed: videoFeedReducer,
   sideMenu: sideMenuReducer,
-  moment: momentReducer,
-  placeholder: placeholderReducer,
 });
 
 export default reducer;
