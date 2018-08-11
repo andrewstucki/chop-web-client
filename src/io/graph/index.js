a21a2d4652a24f2a6eab1a4f257fccee21f62a35
Kenny Heaton <kennyheaton@gmail.com>
2018-08-11 13:45:36 -0500

    Formating graphQL responce

--- src/io/graph/index.js
+++ src/io/graph/index.js
@@ -1,6 +1,6 @@
 // @flow
 import graphqlJs from 'graphql.js';
-import { SET_INIT_DATA, GET_INIT_DATA } from '../../feed/dux';
+import { GET_INIT_DATA, setInitData } from '../../feed/dux';
 
 class GraphQlActor {
   storeDispatch: (action: any) => void
@@ -54,11 +54,34 @@ class GraphQlActor {
 
   getInitialData () {
     this.getAll()
-      .then(data => {
-        this.storeDispatch({
-          type: SET_INIT_DATA,
-          data: data.data,
-        });
+      .then(payload => {
+        this.storeDispatch(
+          setInitData(
+            {
+              event: {
+                startTime: payload.data.currentEvent.eventStartTime,
+                id: payload.data.currentEvent.eventTimeId,
+                timezone: payload.data.currentEvent.eventTimezone,
+                title: payload.data.currentEvent.title,
+              },
+              video: payload.data.currentEvent.video,
+              organization: payload.data.currentOrganization,
+              channels: payload.data.currentFeeds,
+              user: {
+                id: payload.data.currentProfile.id,
+                name: payload.data.currentProfile.nickname,
+                avatar: payload.data.currentProfile.avatar,
+                pubnubAccessKey: payload.data.currentProfile.pubnub_access_key,
+                pubnubToken: payload.data.currentProfile.pubnub_token,
+                role: payload.data.currentProfile.role,
+              },
+              pubnubKeys: {
+                publish: payload.data.pubnubKeys.publish_key,
+                subscribe: payload.data.pubnubKeys.subscribe_key,
+              },
+            }
+          )
+        );
       });
   }
 
--- test/unit/io/graphql.test.js
+++ test/unit/io/graphql.test.js
@@ -20,8 +20,64 @@ describe('Test GraphQL', () => {
     expect(storeDispatch.mock.calls[0][0]).toEqual(
       {
         type: 'SET_INIT_DATA',
-        data: testData.data,
-      }
+        event: {
+          startTime: 1531864800,
+          id: 334494,
+          timezone: 'America/Chicago',
+          title: 'Fake Event',
+        },
+        video: {
+          type: 'StandardEmbed',
+          url: 'https://www.youtube.com/embed/bz2kN31m_S0',
+        },
+        channels: [
+          {
+            direct: false,
+            id: '123456',
+            name: 'public',
+            users: null,
+          },
+          {
+            direct: false,
+            id: '123456',
+            name: 'host',
+            users: null,
+          },
+          {
+            direct: false,
+            id: '123456',
+            name: 'request',
+            users: null,
+          },
+          {
+            direct: false,
+            id: '123456',
+            name: 'command',
+            users: null,
+          },
+        ],
+        organization: {
+          id: 2,
+          name: 'Life.Church',
+        },
+        user: {
+          avatar: null,
+          id: 123456,
+          name: 'Bobby G.',
+          pubnubAccessKey: '123456',
+          pubnubToken: '1533912921585',
+          role: {
+            label: 'HOST',
+            permissions: [
+              'all-the-things',
+            ],
+          },
+        },
+        pubnubKeys: {
+          publish: 'pub-c-1d485d00-14f5-4078-9ca7-19a6fe6411a7',
+          subscribe: 'sub-c-1dc5ff9a-86b2-11e8-ba2a-d686872c68e7',
+        },
+      },
     );
   });
 });
