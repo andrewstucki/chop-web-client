ca2c448d8e201effb625e3f918aaaa14cb71e05f
David Fedor <david.fedor@life.church>
2018-10-07 13:15:36 -0500

    rename chopStyles to styles

--- src/chop/chop.jsx
+++ src/chop/chop.jsx
@@ -11,36 +11,36 @@ import PopUpModal from '../popUpModal';
 import ReactionButton from '../reactionButton';
 
 import '../../assets/global.css';
-import chopStyles from './styles.css';
+import styles from './styles.css';
 
 class ChopContainer extends React.Component {
 
   render () {
-    var wrapperstyle = chopStyles.wrapper;
+    var wrapperstyle = styles.wrapper;
     if (this.props.focused) {
       var iPhone = !!navigator.platform && /iPhone/.test(navigator.platform);
       if (iPhone) {
-        wrapperstyle = chopStyles.wrapperfocused_generic_iphone;
+        wrapperstyle = styles.wrapperfocused_generic_iphone;
         // using info from https://51degrees.com/blog/device-detection-for-apple-iphone-and-ipad
         if (window.screen.height / window.screen.width == 812 / 375 && window.devicePixelRatio == 3)
-          wrapperstyle = chopStyles.wrapperfocused_iphonex;
+          wrapperstyle = styles.wrapperfocused_iphonex;
         if (window.screen.height / window.screen.width == 736 / 414 && window.devicePixelRatio == 3)
-          wrapperstyle = chopStyles.wrapperfocused_iphone678plus;  // iPhone 6+/6s+/7+/8+
+          wrapperstyle = styles.wrapperfocused_iphone678plus;  // iPhone 6+/6s+/7+/8+
         if (window.screen.height / window.screen.width == 667 / 375 && window.devicePixelRatio == 2)
-          wrapperstyle = chopStyles.wrapperfocused_iphone678;  // 6/6s/7/8
+          wrapperstyle = styles.wrapperfocused_iphone678;  // 6/6s/7/8
       }
     }
 
     return (
       <div id="wrapper" className={wrapperstyle}>
         <SideMenu />
-        <div className={chopStyles.chop}>
+        <div className={styles.chop}>
           <PopUpModal />
           <NavBar />
           <VideoFeed />
           <Feed />
           <Placeholder />
-          <div className={chopStyles.inputs}>
+          <div className={styles.inputs}>
             <Chat />
             <ReactionButton />
           </div>
