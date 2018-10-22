f6f0bff464c242fbfc74ad734aaab613561d31e0
Michael Haglund <michael.haglund@life.church>
2018-10-22 10:00:27 -0500

    only notify bugnsnag in staging/production

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,11 @@ import tagManagerMiddleware from './middleware/tagmanager-middleware';
 import bugsnag from 'bugsnag-js';
 import createPlugin from 'bugsnag-react';
 
-const bugsnagClient = bugsnag('2403ac729529750d296e1e4ee022f7dc');
+const bugsnagClient = bugsnag({
+  apiKey: '2403ac729529750d296e1e4ee022f7dc',
+  notifyReleaseStages: [ 'production', 'staging' ]
+});
+
 const ErrorBoundary = bugsnagClient.use(createPlugin(React));
 
 const actorMiddlewareApplied = actorMiddleware(
