3a659a1f5028ee802bf54b5be6bbb38ad7f4edf4
Michael Haglund <michael.haglund@life.church>
2018-12-14 11:58:35 -0600

    fix global.css not being bundled

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
