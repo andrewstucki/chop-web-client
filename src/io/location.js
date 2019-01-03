e7deb8cf791fa96b411eead61d1b81d865f188cb
Kenny Heaton <kenny.heaton@life.church>
2019-01-03 11:05:12 -0600

    Fix lint

--- src/io/location.js
+++ src/io/location.js
@@ -8,7 +8,7 @@ export default class Location {
       hostname.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/) // Any IP Address
     ) {
       if (hash.length > 1) {
-        return hash.substr(1) // First charater in hash is always '#'
+        return hash.substr(1); // First charater in hash is always '#'
       } else {
         return 'digerati.chopdev.com';
       }
