15ec685bb65696a6e3b77ef9c47ef33313097784
Eric <ericpaullutz@gmail.com>
2019-01-21 16:31:08 -0600

    change avatar image size

--- src/avatar/imageAvatar.jsx
+++ src/avatar/imageAvatar.jsx
@@ -4,7 +4,7 @@ import styles from './style.css';
 const ImageAvatar = ({url}) => (
   <div
     className={styles.icon}
-    style={{backgroundImage: `url(${url})`, backgroundSize: '36px 36px'}}
+    style={{backgroundImage: `url(${url})`, backgroundSize: 'cover'}}
   >
   </div>
 );
