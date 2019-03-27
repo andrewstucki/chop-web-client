b511bd60c61b85f8c5a69a071b3138f0e59f56a1
Michael Haglund <michael.haglund@life.church>
2019-03-27 10:48:44 -0500

    don't render an empty videoHeader

--- src/videoFeed/videoHeader/videoHeader.jsx
+++ src/videoFeed/videoHeader/videoHeader.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Countdown, { zeroPad } from 'react-countdown-now';
 import { Wrapper, EventInfo, Title, Description, CountdownWrapper } from './styles';
+import { isEmpty } from '../../util';
 
 type VideoHeaderProps = {
   title: string,
@@ -11,19 +12,25 @@ type VideoHeaderProps = {
 
 const MemoziedCountdown = React.memo(Countdown);
 
-const VideoHeader = ({ title, description, endTime }:VideoHeaderProps) => (
-  <Wrapper data-testid='videoHeader'>
-    <EventInfo>
-      <Title data-testid='videoHeader-title'>{title}</Title>
-      <Description data-testid='videoHeader-description'>{description}</Description>
-    </EventInfo>
-    { endTime > 0 &&
-      <CountdownWrapper>
-        <MemoziedCountdown date={endTime} renderer={countdownRenderer}/>
-      </CountdownWrapper>
-    }
-  </Wrapper>
-);
+const VideoHeader = ({ title, description, endTime }:VideoHeaderProps) => {
+  if (isEmpty(title)) {
+    return null;
+  } else {
+    return (
+      <Wrapper data-testid='videoHeader'>
+        <EventInfo>
+          <Title data-testid='videoHeader-title'>{title}</Title>
+          <Description data-testid='videoHeader-description'>{description}</Description>
+        </EventInfo>
+        {endTime > 0 &&
+        <CountdownWrapper>
+          <MemoziedCountdown date={endTime} renderer={countdownRenderer}/>
+        </CountdownWrapper>
+        }
+      </Wrapper>
+    );
+  }
+};
 
 const countdownRenderer = ({ hours, minutes, seconds }) => (
   <span data-testid='videoHeader-countdown'>{hours > 0 ? `${hours}:` : ''}{zeroPad(minutes)}:{zeroPad(seconds)}</span>
