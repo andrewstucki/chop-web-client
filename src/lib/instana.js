3e3a727a17249f776e1382087f38512c2e3d7775
Kenny Heaton <kenny.heaton@life.church>
2019-08-14 18:35:19 +0000

    Add Instana ;)

---
+++ src/lib/instana.js
@@ -0,0 +1,10 @@
+(function (c,e,f,k,g,h,b,a,d) {
+c[g] || (c[g] = h,b = c[h] = function () {
+  b.q.push(arguments);
+},b.q = [],b.l = 1 * new Date,a = e.createElement(f),a.async = 1,
+a.src = k,a.setAttribute('crossorigin', 'anonymous'),d = e.getElementsByTagName(f)[0],
+d.parentNode.insertBefore(a,d))
+;})(window,document,'script',
+  '//eum.instana.io/eum.min.js','InstanaEumObject','ineum');
+ineum('reportingUrl', 'https://eum-us-west-2.instana.io');
+ineum('key', '59fC31wxS2OrLbybrVH40g');
